9320dabefb7829c6f02eaac586fb8568c0f82c53
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2026-03-02 20:26:55 +0100

    Feature: Variables de favicon importadas

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -43,7 +43,11 @@
     <meta name="theme-color" content="#000000">
 
     <link rel="icon" href="{{ asset('favicon.ico') }}">
-
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::asset('resources/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::asset('resources/img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ Vite::asset('resources/img/favicon/site.webmanifest') }}">
+    
     <link rel="preconnect" href="https://use.typekit.net" crossorigin>
     <link rel="preconnect" href="https://p.typekit.net" crossorigin>
     <link rel="preconnect" href="https://fonts.googleapis.com">
