7312d9d8a1ee077178e4c717c3083119da900f62
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2026-03-03 11:28:27 +0100

    chore(i18n): añadir traducciones es para auth/passwords

---
+++ lang/es/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos. Inténtalo de nuevo en :seconds segundos.',
+];
---
+++ lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset'     => '¡Tu contraseña ha sido restablecida!',
+    'sent'      => 'Hemos enviado el enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor, espera antes de reintentar.',
+    'token'     => 'El token de restablecimiento no es válido.',
+    'user'      => 'No encontramos un usuario con ese email.',
+];
