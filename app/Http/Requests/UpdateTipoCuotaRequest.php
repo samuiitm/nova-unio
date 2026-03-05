51949ee0d345be8c9956731bf77d00050f2c7033
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2026-03-05 19:07:11 +0100

    fix update tipo cuota request

--- app/Http/Requests/UpdateTipoCuotaRequest.php
+++ app/Http/Requests/UpdateTipoCuotaRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreTipoCuotaRequest extends FormRequest
+class UpdateTipoCuotaRequest extends FormRequest
 {
     public function authorize(): bool { return true; }
 
